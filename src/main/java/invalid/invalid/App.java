80379de1ea8af841aef169c4f654661f925fbbba
John Croasdale <jcroasii@gmail.com>
2018-02-15 20:09:54 -0600

    initial commit

---
+++ src/main/java/invalid/invalid/App.java
@@ -0,0 +1,13 @@
+package invalid.invalid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
